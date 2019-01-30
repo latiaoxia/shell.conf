091a49dd97b775da72e53036268d2810a1c5cab3
Bert Xu <yili.xu@nxp.com>
2019-01-30 12:50:32 +0800

    fix some git clone error

--- bin/autoscript.sh
+++ bin/autoscript.sh
@@ -25,12 +25,12 @@ $install_cmd zsh git python python3 python-pip python3-pip
 
 echo "configuring zsh....."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-completions $ZSH_CUSTOM/custom/plugins/zsh-completions
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-[ -d $HOME/bin/] || mkdir $HOME/bin
+[ -d $HOME/bin/ ] || mkdir $HOME/bin
 
 echo "installing nvim...."
 curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -o $HOME/bin/nvim
